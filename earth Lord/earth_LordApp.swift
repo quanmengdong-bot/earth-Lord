c0fe027669fdded44a4532de2e49c6aaf85af94b
1 <1>
2025-12-24 17:57:54 -0800

    Initial Commit

---
+++ earth Lord/earth_LordApp.swift
@@ -0,0 +1,17 @@
+//
+//  earth_LordApp.swift
+//  earth Lord
+//
+//  Created by 富尔喜悦的孟冬 on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct earth_LordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
